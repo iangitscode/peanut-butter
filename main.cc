c61a78ca5fdc1d35897920975f85d19bd1b4fbe2
ShriGrover <grovershri@gmail.com>
2017-02-19 22:57:17 -0800

    Added windows.h
    Added method to allow usleep for windows

--- main.cc
+++ main.cc
@@ -1,12 +1,26 @@
 #include <SFML/Graphics.hpp>
-#include <unistd.h>
+//#include <unistd.h>
 #include <iostream>
 #include <vector>
 #include "Object.h"
 #include "Paddle.h"
+#include <Windows.h>
 
 extern std::vector<Object*> objects;
 std::vector<Object*> objects;
+
+void usleep(__int64 usec)
+{
+	HANDLE timer;
+	LARGE_INTEGER ft;
+
+	ft.QuadPart = -(10 * usec); // Convert to 100 nanosecond interval, negative value indicates relative time
+
+	timer = CreateWaitableTimer(NULL, TRUE, NULL);
+	SetWaitableTimer(timer, &ft, 0, NULL, NULL, 0);
+	WaitForSingleObject(timer, INFINITE);
+	CloseHandle(timer);
+}
 
 int main()
 {
